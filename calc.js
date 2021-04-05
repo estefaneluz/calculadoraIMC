49fcec36be8ce89d2450333ce2e925b525be78a0
Estefane <estefanecortes@gmail.com>
2021-04-05 11:44:13 -0300

    mensagens personalizadas de acordo com o IMC

--- calc.js
+++ calc.js
@@ -5,5 +5,26 @@ document.getElementById("btnCalc").addEventListener('click', function(event){
   const peso = parseFloat(document.getElementById("peso").value); 
   const calculo = peso/(altura*altura); 
 
-  document.getElementById("resultado").innerHTML = "Total: "+calculo.toFixed(2); 
+
+  if(calculo <= 18.5) {
+    document.getElementById("resultado").innerHTML = "Total: "+calculo.toFixed(2)+ ". Você está abaixo do peso."
+  }
+  else if(calculo> 18.5 && calculo<=24.9) {
+    document.getElementById("resultado").innerHTML = "Total: "+calculo.toFixed(2)+ ". Você está com o peso ideal."
+  }
+  else if(calculo>24.9 && calculo <=29.9) {
+    document.getElementById("resultado").innerHTML = "Total: "+calculo.toFixed(2)+ ". Você está com Sobrepeso."
+  }
+  else if(calculo>29.9 && calculo <=34.9) {
+    document.getElementById("resultado").innerHTML = "Total: "+calculo.toFixed(2)+ ". Você está com Obesidade grau 1."
+  }
+  else if(calculo>34.9 && calculo <=39.9) {
+    document.getElementById("resultado").innerHTML = "Total: "+calculo.toFixed(2)+ ". Você está com Obesidade grau 2."
+  }
+
+  else if(calculo>39.9) {
+    document.getElementById("resultado").innerHTML = "Total: "+calculo.toFixed(2)+ ". Você está com Obesidade grau 3."
+  }
+
+
 })
